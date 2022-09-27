e12678fe844b6cddd1d221dca62aad4b59703b8a
Fabricio-derp <derp.fp@gmail.com>
2022-09-27 06:32:59 -0300

    Fix tv layout and adding git ignore role

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
 export const environment = {
-  production: true
+  production: true,
+  apiKey: 'f98bb1dfa8a65870fe4fc9567cd30456',
+  baseApiUrl: 'https://api.themoviedb.org/3/',
+  baseImgUrl: 'https://image.tmdb.org/t/p/',
+  baseStreamUrl: 'https://embed.warezcdn.com/'
 };
