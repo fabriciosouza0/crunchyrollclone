15f72130c569a8bea094e833c879afdb71ecd9d0
Fabricio <50778242+fabriciosouza0@users.noreply.github.com>
2024-09-25 09:41:12 -0300

    Refatc: mudança na formatação

--- src/app/components/pages/discover/discover-routing.module.ts
+++ src/app/components/pages/discover/discover-routing.module.ts
@@ -1,24 +1,38 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
 
-import { AnimeComponent } from './anime/anime.component';
-import { DiscoverComponent } from './discover.component';
-import { MovieComponent } from './movie/movie.component';
-import { TvComponent } from './tv/tv.component';
+import { AnimeComponent } from "./anime/anime.component";
+import { DiscoverComponent } from "./discover.component";
+import { MovieComponent } from "./movie/movie.component";
+import { TvComponent } from "./tv/tv.component";
 
 const discoverRoutes: Routes = [
-    {
-        path: '', title: 'DatMovie - Discover', component: DiscoverComponent,
-        children: [
-            { path: 'filmes', title: 'DatMovie - Discover Movies', component: MovieComponent },
-            { path: 'series', title: 'DatMovie - Discover Series', component: TvComponent },
-            { path: 'animes', title: 'DatMovie - Discover Animes', component: AnimeComponent },
-        ]
-    }
+  {
+    path: "",
+    title: "DatMovie - Discover",
+    component: DiscoverComponent,
+    children: [
+      {
+        path: "filmes",
+        title: "DatMovie - Discover Filmes",
+        component: MovieComponent,
+      },
+      {
+        path: "series",
+        title: "DatMovie - Discover Séries",
+        component: TvComponent,
+      },
+      {
+        path: "animes",
+        title: "DatMovie - Discover Animes",
+        component: AnimeComponent,
+      },
+    ],
+  },
 ];
 
 @NgModule({
-    imports: [RouterModule.forChild(discoverRoutes)],
-    exports: [RouterModule]
+  imports: [RouterModule.forChild(discoverRoutes)],
+  exports: [RouterModule],
 })
-export class DiscoverRoutingModule { }
+export class DiscoverRoutingModule {}
