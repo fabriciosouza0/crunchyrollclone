86fbace907c1cec50daeeef664cfe11ffad53b85
Fabricio <50778242+fabriciosouza0@users.noreply.github.com>
2024-09-25 09:36:43 -0300

    Refact: reajuste da formatação

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,22 +1,34 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-import { LimboComponent } from './components/limbo/limbo.component';
-import { HomeComponent } from './components/pages/home/home.component';
-import { SearchComponent } from './components/pages/search/search.component';
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+import { LimboComponent } from "./components/limbo/limbo.component";
+import { HomeComponent } from "./components/pages/home/home.component";
+import { SearchComponent } from "./components/pages/search/search.component";
 
 const appRoutes: Routes = [
-    { path: '', title: 'DatMovie - Home', component: HomeComponent },
-    { path: 'search', title: 'DatMovie - Search', component: SearchComponent },
-    { path: 'discover', loadChildren: () => import('./components/pages/discover/discover.module').then(m => m.DiscoverModule) },
-    { path: 'detalhes/:type/:id', loadChildren: () => import('./components/pages/details/details.module').then(m => m.DetailsModule) },
-    { path: '**', title: 'DatMovie - Limbo', component: LimboComponent },
-]
+  { path: "", title: "DatMovie - Home", component: HomeComponent },
+  { path: "search", title: "DatMovie - Search", component: SearchComponent },
+  {
+    path: "discover",
+    loadChildren: () =>
+      import("./components/pages/discover/discover.module").then(
+        (m) => m.DiscoverModule
+      ),
+  },
+  {
+    path: "detalhes/:type/:id",
+    loadChildren: () =>
+      import("./components/pages/details/details.module").then(
+        (m) => m.DetailsModule
+      ),
+  },
+  { path: "**", title: "DatMovie - Limbo", component: LimboComponent },
+];
 
 @NgModule({
-    imports: [
-        RouterModule.forRoot(appRoutes, { scrollPositionRestoration: 'top' })
-    ],
-    exports: [RouterModule],
-    providers: [],
+  imports: [
+    RouterModule.forRoot(appRoutes, { scrollPositionRestoration: "top" }),
+  ],
+  exports: [RouterModule],
+  providers: [],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
