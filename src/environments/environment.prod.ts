0e17aaca0f157d9b071db6401c13201299888059
Fabricio <50778242+fabriciosouza0@users.noreply.github.com>
2024-09-25 12:00:55 -0300

    Removendo arquivos de environment do repositório

--- src/environments/environment.prod.ts
+++
@@ -1,7 +0,0 @@
-export const environment = {
-  production: true,
-  apiKey: 'f98bb1dfa8a65870fe4fc9567cd30456',
-  baseApiUrl: 'https://api.themoviedb.org/3/',
-  baseImgUrl: 'https://image.tmdb.org/t/p/',
-  baseStreamUrl: 'https://embed.warezcdn.com/'
-};
